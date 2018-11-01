ce7558282e2c4415c267d90a427f5c23fc0cc13c
Luisa Fernanda Vaca Correa <luisa.vaca@globant.com>
2018-11-01 14:32:43 -0500

    ESPN-DEMO can edit field

--- src/components/containers/Student/Student.js
+++ src/components/containers/Student/Student.js
@@ -12,8 +12,8 @@ class Student extends React.Component {
             const id    = item.id,
                   name  = item.name,
                   email = item.email;
-            const studentItemName = item.edit ? <p className="student-item"><input type="text" value={name}></input></p> :  <p className="student-item">{name}</p>;
-            const studentItemEmail = item.edit ? <p className="student-item"><input type="text" value={email}></input></p> : <p className="student-item">{email}</p>;
+            const studentItemName = item.edit ? <p className="student-item"><input type="text" placeholder={name} /></p> :  <p className="student-item">{name}</p>;
+            const studentItemEmail = item.edit ? <p className="student-item"><input type="text" placeholder={email}></input></p> : <p className="student-item">{email}</p>;
             const saveBtn = item.edit ?  <p className="student-item save"><button>Save</button></p> : null;
             return(
                 <div className="student-row" key={id}>
